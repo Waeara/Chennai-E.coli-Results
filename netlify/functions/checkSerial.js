4e722cd52a086f58054652a98e3a91e2177fefdf
Asaf Pras <63721738+Waeara@users.noreply.github.com>
2023-10-02 16:12:01 +0300

    Trying to fix the problem

--- netlify/functions/checkSerial.js
+++ netlify/functions/checkSerial.js
@@ -3,32 +3,35 @@ const fetch = require('node-fetch');
 exports.handler = async function(event, context) {
   const serial = event.queryStringParameters.serial;
 
-  // Construct the GAS URL
-  const GAS_URL = `https://script.google.com/macros/s/AKfycbxHPILzJO1EEpb1YO9RFsFP6O4g6seobnZqasb2UI7ibBqRqjEgeTyj74qKELwVJz_2Ww/exec?serial=${serial}`;
+  if (!serial) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Serial parameter is missing" })
+    };
+  }
 
-  // Log the URL for debugging purposes
-  console.log("Fetching URL:", GAS_URL);
+  const GAS_URL = `https://script.google.com/macros/s/AKfycbzl6CeBZ6NX8sX5uNB2dA6b4twwdcOpVmqSnOPEgDoontRq7neuIQwhogJdyCvfaxRt8Q/exec?serial=${serial}`;
 
   try {
     const response = await fetch(GAS_URL);
-    
-    // Log any potential fetch issues
-    if (!response.ok) {
-       console.error("Error fetching GAS:", response.statusText);
-       throw new Error(`HTTP error! Status: ${response.status}`);
+    const data = await response.json();
+
+    if (data.error) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: data.error })
+      };
     }
-    
-    const data = await response.text();
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: data })
+      body: JSON.stringify({ message: data.message })
     };
   } catch (error) {
-    console.error("Error:", error);
     return {
       statusCode: 500,
       body: JSON.stringify({ message: "Internal Server Error" })
     };
   }
 };
+
