e3c81c088d9801b0d9843f2336468541bec40357
Asaf Pras <63721738+Waeara@users.noreply.github.com>
2023-10-02 15:26:35 +0300

    Add files via upload

---
+++ netlify/functions/checkSerial.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const serial = event.queryStringParameters.serial;
+  const GAS_URL = `https://script.google.com/macros/s/AKfycbxHPILzJO1EEpb1YO9RFsFP6O4g6seobnZqasb2UI7ibBqRqjEgeTyj74qKELwVJz_2Ww/exec?serial=${serial}`;
+
+  try {
+    const response = await fetch(GAS_URL);
+    const data = await response.text();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: data })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" })
+    };
+  }
+};
